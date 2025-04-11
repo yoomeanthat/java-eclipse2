cda6537dd3d7dc8fb6402ec9262d97751b6d5d51
yoomeanthat <164070862+yoomeanthat@users.noreply.github.com>
2025-04-11 12:18:33 +0900

    Add files via upload

---
+++ day02Prac/src/main/java/day02Prac/Test.java
@@ -0,0 +1,16 @@
+package day02Prac;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Movie m = new Movie("베테랑2", "118분", "황정민");
+		
+		System.out.println(m);
+		System.out.println(m.toString()); // toString()매서드 호출됨, 생략가능한 메서드
+		
+		System.out.println(m.getActor());
+		System.out.println(m.getName());
+		System.out.println(m.getRunningtime());
+	}
+
+}
